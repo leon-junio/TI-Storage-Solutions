d1cd37f115feaede1a13e89135ce4e5a26f3b5fa
Leon Júnio Martins <juniohuy2gamer@gmail.com>
2022-03-29 15:52:48 -0300

    Criação de Objetos Iniciais

---
+++ Codigo/backend/src/main/java/model/Estoque.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Estoque {
+
+	public Estoque() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Codigo/backend/src/main/java/model/Fornecedor.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Fornecedor {
+
+	public Fornecedor() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
