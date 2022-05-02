841372ec75e43c0b73e569f54e37e50af35a7171
Leon Júnio Martins <juniohuy2gamer@gmail.com>
2022-05-02 17:03:36 -0300

    Novo dao de conexão

--- Codigo/backend/src/main/java/dao/DAO.java
+++ Codigo/backend/src/main/java/dao/DAO.java
@@ -3,78 +3,46 @@ package dao;
 import java.sql.*;
 
 public class DAO {
-
-	private static Connection conexao;
-	private static String driverName = "org.postgresql.Driver";
-	private static String serverName = "localhost";
-	private static String mydatabase = "Aplicacao";
-	private static int porta = 5432;
-	private static String username = "ti2cc";
-	private static String password = "ti@cc";
-
+protected Connection conexao;
+	
 	public DAO() {
 		conexao = null;
 	}
-
-	private static boolean conectar() {
+	
+	public boolean conectar() {
+		String driverName = "org.postgresql.Driver";                    
+		String serverName = "localhost";
+		String mydatabase = "aplicacao";
+		int porta = 5432;
+		String url = "jdbc:postgresql://" + serverName + ":" + porta +"/" + mydatabase;
+		String username = "ti2cc";
+		String password = "ti@cc";
 		boolean status = false;
-		String url = "jdbc:postgresql://" + serverName + ":" + porta + "/" + mydatabase;
+
 		try {
 			Class.forName(driverName);
 			conexao = DriverManager.getConnection(url, username, password);
-			status = (conexao != null);
+			status = (conexao == null);
 			System.out.println("Conexão efetuada com o postgres!");
-		} catch (ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) { 
 			System.err.println("Conexão NÃO efetuada com o postgres -- Driver não encontrado -- " + e.getMessage());
 		} catch (SQLException e) {
 			System.err.println("Conexão NÃO efetuada com o postgres -- " + e.getMessage());
 		}
+
 		return status;
 	}
-
-	public static boolean close() {
-		boolean info = false;
+	
+	public boolean close() {
+		boolean status = false;
+		
 		try {
 			conexao.close();
-			info = true;
+			status = true;
 		} catch (SQLException e) {
 			System.err.println(e.getMessage());
 		}
-		return info;
-	}
-
-	public static boolean getConexao() {
-		boolean info = false;
-		try {
-			if (conexao == null) {
-				info = conectar();
-			}else {
-				info = true;
-			}
-		} catch (Exception e) {
-			System.err.println(e.getMessage());
-		}
-		return info;
-	}
-
-	public static boolean conectionState() {
-		return conexao != null;
-	}
-
-	public static Statement createSt() throws SQLException {
-		Statement st = null;
-		if (getConexao()) {
-			st = conexao.createStatement();
-		}
-		return st;
-	}
-	
-	public static Statement createStIs() throws SQLException {
-		Statement st = null;
-		if (getConexao()) {
-			st = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
-		}
-		return st;
+		return status;
 	}
 
 }
