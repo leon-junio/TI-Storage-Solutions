8d75e0bb71c881e1e388efbdf8492242da918708
Leon Júnio Martins <juniohuy2gamer@gmail.com>
2022-05-02 16:23:35 -0300

    Criação dos objetos de cliente e endereco

--- Codigo/backend/src/main/java/model/Cliente.java
+++ Codigo/backend/src/main/java/model/Cliente.java
@@ -3,7 +3,51 @@ package model;
 public class Cliente {
 
 	public Cliente() {
-		// TODO Auto-generated constructor stub
+		
 	}
+	
+	private int idCliente;
+	private String nome,email,usuario,senha;
+	
+	
+	
+	public Cliente(int idCliente, String nome, String email, String usuario, String senha) {
+		this.idCliente = idCliente;
+		this.nome = nome;
+		this.email = email;
+		this.usuario = usuario;
+		this.senha = senha;
+	}
+	public int getIdCliente() {
+		return idCliente;
+	}
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
 
 }
--- Codigo/backend/src/main/java/model/Endereco.java
+++ Codigo/backend/src/main/java/model/Endereco.java
@@ -3,7 +3,86 @@ package model;
 public class Endereco {
 
 	public Endereco() {
-		// TODO Auto-generated constructor stub
+
+	}
+
+	public Endereco(int idEndereco, int numero, String cep, String estado, String pais, String rua, String bairro,
+			String cidade) {
+		this.idEndereco = idEndereco;
+		this.numero = numero;
+		this.cep = cep;
+		this.estado = estado;
+		this.pais = pais;
+		this.rua = rua;
+		this.bairro = bairro;
+		this.cidade = cidade;
+	}
+
+	private int idEndereco, numero;
+	private String cep, estado, pais, rua, bairro, cidade;
+
+	public int getIdEndereco() {
+		return idEndereco;
+	}
+
+	public void setIdEndereco(int idEndereco) {
+		this.idEndereco = idEndereco;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
 	}
 
 }
