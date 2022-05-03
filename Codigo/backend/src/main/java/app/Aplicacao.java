970caacd166dc029a0ef79104b6dc1180fd0d81c
Leon Júnio Martins <juniohuy2gamer@gmail.com>
2022-05-03 13:20:50 -0300

    03/05/2022 Preparando para a criação do webservice

--- Codigo/backend/src/main/java/app/Aplicacao.java
+++ Codigo/backend/src/main/java/app/Aplicacao.java
@@ -1,9 +1,38 @@
 package app;
 
+import static spark.Spark.*;
+
+import service.FornecedorService;
+import service.*;
+
+
 public class Aplicacao {
+	
+	private static ProdutoService produtoService = new ProdutoService();
+	private static ClienteService clienteService = new ClienteService();
+	private static FornecedorService fornecedorService = new FornecedorService();
+	private static EstoqueService estoqueService = new EstoqueService();
+	
+    public static void main(String[] args) {
+        port(6789);
+        
+        staticFiles.location("/public");
+        
+        //CRIAR O WEBSERVICE AQUI!
+        
+        /*
+        post("/produto/insert", (request, response) -> produtoService.insert(request, response));
 
-	public Aplicacao() {
-		// TODO Auto-generated constructor stub
-	}
+        get("/produto/:id", (request, response) -> produtoService.get(request, response));
+        
+        get("/produto/list/:orderby", (request, response) -> produtoService.getAll(request, response));
 
+        get("/produto/update/:id", (request, response) -> produtoService.getToUpdate(request, response));
+        
+        post("/produto/update/:id", (request, response) -> produtoService.update(request, response));
+           
+        get("/produto/delete/:id", (request, response) -> produtoService.delete(request, response));
+         */
+             
+    }
 }
