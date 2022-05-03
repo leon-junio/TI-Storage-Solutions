4bdf9b862500d01aa96c35dfb62483f8f9fb33c7
Leon Júnio Martins <juniohuy2gamer@gmail.com>
2022-05-03 11:29:51 -0300

    03/05/2022 DAOS de endereço e estoque prontos para uso

--- Codigo/backend/src/main/java/dao/DAOEndereco.java
+++ Codigo/backend/src/main/java/dao/DAOEndereco.java
@@ -1,9 +1,122 @@
 package dao;
 
-public class DAOEndereco {
+import model.Endereco;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+public class DAOEndereco extends DAO {
 	public DAOEndereco() {
-		// TODO Auto-generated constructor stub
+		super();
+		conectar();
 	}
 
+	public void finalize() {
+		close();
+	}
+
+	public boolean insert(Endereco endereco) {
+		boolean status = false;
+		try {
+			String sql = "INSERT INTO endereco (numero,cep,estado,pais,rua,bairro,cidade) " + "VALUES (?,?,?,?,?,?,?);";
+			PreparedStatement st = conexao.prepareStatement(sql);
+			st.setInt(1, endereco.getNumero());
+			st.setString(2, endereco.getCep());
+			st.setString(3, endereco.getEstado());
+			st.setString(4, endereco.getPais());
+			st.setString(5, endereco.getRua());
+			st.setString(6, endereco.getBairro());
+			st.setString(7, endereco.getCidade());
+			st.executeUpdate();
+			st.close();
+			status = true;
+		} catch (SQLException u) {
+			throw new RuntimeException(u);
+		}
+		return status;
+	}
+
+	public Endereco get(int id) {
+		Endereco endereco = null;
+
+		try {
+			Statement st = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			String sql = "SELECT * FROM endereco WHERE id=" + id;
+			ResultSet rs = st.executeQuery(sql);
+			if (rs.next()) {
+				endereco = new Endereco(rs.getInt("idEndereco"),rs.getInt("numero"), rs.getString("cep"), rs.getString("estado"),
+						rs.getString("pais"), rs.getString("rua"),rs.getString("bairro"),rs.getString("cidade"));
+			}
+			st.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return endereco;
+	}
+
+	public List<Endereco> get() {
+		return get("");
+	}
+
+	public List<Endereco> getOrderByID() {
+		return get("id");
+	}
+
+
+	private List<Endereco> get(String orderBy) {
+		List<Endereco> enderecos = new ArrayList<Endereco>();
+
+		try {
+			Statement st = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			String sql = "SELECT * FROM endereco" + ((orderBy.trim().length() == 0) ? "" : (" ORDER BY " + orderBy));
+			ResultSet rs = st.executeQuery(sql);
+			while (rs.next()) {
+				Endereco p = new Endereco(rs.getInt("idEndereco"),rs.getInt("numero"), rs.getString("cep"), rs.getString("estado"),
+						rs.getString("pais"), rs.getString("rua"),rs.getString("bairro"),rs.getString("cidade"));
+				enderecos.add(p);
+			}
+			st.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return enderecos;
+	}
+
+	public boolean update(Endereco endereco) {
+		boolean status = false;
+		try {
+			String sql = "UPDATE endereco SET numero = ?,cep = ?,estado = ?,pais = ?,rua = ?,bairro = ?,cidade = ? WHERE idEndereco = ?;";
+			PreparedStatement st = conexao.prepareStatement(sql);
+			st.setInt(1, endereco.getNumero());
+			st.setString(2, endereco.getCep());
+			st.setString(3, endereco.getEstado());
+			st.setString(4, endereco.getPais());
+			st.setString(5, endereco.getRua());
+			st.setString(6, endereco.getBairro());
+			st.setString(7, endereco.getCidade());
+			st.setInt(8, endereco.getIdEndereco());
+			st.executeUpdate();
+			st.close();
+			status = true;
+		} catch (SQLException u) {
+			throw new RuntimeException(u);
+		}
+		return status;
+	}
+
+	public boolean delete(int id) {
+		boolean status = false;
+		try {
+			Statement st = conexao.createStatement();
+			st.executeUpdate("DELETE FROM endereco WHERE id = " + id);
+			st.close();
+			status = true;
+		} catch (SQLException u) {
+			throw new RuntimeException(u);
+		}
+		return status;
+	}
 }
--- Codigo/backend/src/main/java/dao/DAOEstoque.java
+++ Codigo/backend/src/main/java/dao/DAOEstoque.java
@@ -1,9 +1,123 @@
 package dao;
 
-public class DAOEstoque {
+import model.Estoque;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+public class DAOEstoque extends DAO {
 	public DAOEstoque() {
-		// TODO Auto-generated constructor stub
+		super();
+		conectar();
 	}
 
+	public void finalize() {
+		close();
+	}
+
+	public boolean insert(Estoque estoque) {
+		boolean status = false;
+		try {
+			String sql = "INSERT INTO estoque (capacidade,cliente,fornecedor,nome) " + "VALUES (?,?,?,?);";
+			PreparedStatement st = conexao.prepareStatement(sql);
+			st.setInt(1, estoque.getCapacidade());
+			st.setInt(2, estoque.getCliente());
+			st.setInt(3, estoque.getFornecedor());
+			st.setString(4, estoque.getNome());
+			st.executeUpdate();
+			st.close();
+			status = true;
+		} catch (SQLException u) {
+			throw new RuntimeException(u);
+		}
+		return status;
+	}
+
+	public Estoque get(int id) {
+		Estoque estoque = null;
+
+		try {
+			Statement st = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			String sql = "SELECT * FROM estoque WHERE id=" + id;
+			ResultSet rs = st.executeQuery(sql);
+			if (rs.next()) {
+				estoque = new Estoque(rs.getInt("idEstoque"), rs.getInt("capacidade"), rs.getInt("cliente"),
+						rs.getInt("fornecedor"), rs.getString("nome"));
+			}
+			st.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return estoque;
+	}
+
+	public List<Estoque> get() {
+		return get("");
+	}
+
+	public List<Estoque> getOrderByID() {
+		return get("id");
+	}
+
+	public List<Estoque> getOrderByCliente() {
+		return get("cliente");
+	}
+
+	public List<Estoque> getOrderByFornecedor() {
+		return get("fornecedor");
+	}
+
+	private List<Estoque> get(String orderBy) {
+		List<Estoque> estoques = new ArrayList<Estoque>();
+
+		try {
+			Statement st = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			String sql = "SELECT * FROM estoque" + ((orderBy.trim().length() == 0) ? "" : (" ORDER BY " + orderBy));
+			ResultSet rs = st.executeQuery(sql);
+			while (rs.next()) {
+				Estoque p = new Estoque(rs.getInt("idEstoque"), rs.getInt("capacidade"), rs.getInt("cliente"),
+						rs.getInt("fornecedor"), rs.getString("nome"));
+				estoques.add(p);
+			}
+			st.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return estoques;
+	}
+
+	public boolean update(Estoque estoque) {
+		boolean status = false;
+		try {
+			String sql = "UPDATE estoque SET capacidade= ?,cliente= ?,fornecedor= ?,nome= ? WHERE idEstoque = ?;";
+			PreparedStatement st = conexao.prepareStatement(sql);
+			st.setInt(1, estoque.getCapacidade());
+			st.setInt(2, estoque.getCliente());
+			st.setInt(3, estoque.getFornecedor());
+			st.setString(4, estoque.getNome());
+			st.setInt(5, estoque.getIdEstoque());
+			st.executeUpdate();
+			st.close();
+			status = true;
+		} catch (SQLException u) {
+			throw new RuntimeException(u);
+		}
+		return status;
+	}
+
+	public boolean delete(int id) {
+		boolean status = false;
+		try {
+			Statement st = conexao.createStatement();
+			st.executeUpdate("DELETE FROM estoque WHERE id = " + id);
+			st.close();
+			status = true;
+		} catch (SQLException u) {
+			throw new RuntimeException(u);
+		}
+		return status;
+	}
 }
