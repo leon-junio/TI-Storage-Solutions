6393c324ae5e824ca39d6ce408e3b96fb5f4fabd
Leon Júnio Martins <juniohuy2gamer@gmail.com>
2022-05-05 00:53:44 -0300

    04/05/2022 Sistema de geração de tokens aleatorios para usuarios da api

---
+++ Codigo/backend/src/main/java/dao/DAOToken.java
@@ -0,0 +1,84 @@
+package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import utils.HashUtils;
+
+public class DAOToken extends DAO {
+	public DAOToken() {
+		super();
+		conectar();
+	}
+
+	public void finalize() {
+		close();
+	}
+
+	public String insert(String email, String senha) {
+		String token = "";
+		try {
+			String sql = "INSERT INTO StorageSolutionsDB.tokens (email,senha,token) "
+					+ "VALUES (?,?,?);";
+			PreparedStatement st = conexao.prepareStatement(sql);
+			st.setString(1, email);
+			st.setString(2, senha);
+            token = utils.LeonAPI.generateToken(email.length());
+			st.setString(3,token);
+			st.executeUpdate();
+			st.close();
+		} catch (SQLException u) {
+			throw new RuntimeException(u);
+		}
+		return token;
+	}
+
+
+
+	public boolean check(String email,String senha, String token) {
+		boolean resp = false;
+		try {
+			Statement st = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			String sql = "SELECT * FROM StorageSolutionsDB.tokens WHERE email like '" +email+"' and senha like '"+HashUtils.getHashMd5(senha)+"' and token like '"+token+"'" ;
+			ResultSet rs = st.executeQuery(sql);
+			if (rs.next()) {
+				resp = true;
+			}
+			st.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return resp;
+	}
+
+    public boolean check(String token) {
+		boolean resp = false;
+		try {
+			Statement st = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			String sql = "SELECT * FROM StorageSolutionsDB.tokens WHERE token like '"+token+"'" ;
+			ResultSet rs = st.executeQuery(sql);
+			if (rs.next()) {
+				resp = true;
+			}
+			st.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return resp;
+	}
+
+	public boolean delete(int id) {
+		boolean status = false;
+		try {
+			Statement st = conexao.createStatement();
+			st.executeUpdate("DELETE FROM StorageSolutionsDB.tokens WHERE idToken = " + id);
+			st.close();
+			status = true;
+		} catch (SQLException u) {
+			throw new RuntimeException(u);
+		}
+		return status;
+	}
+}
