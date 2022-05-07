1d8d48ab550be359e17044490ef01374a8b3dfdf
Leon Júnio Martins <juniohuy2gamer@gmail.com>
2022-05-07 01:08:52 -0300

    07/05/2022 Serviço de perfil operante

--- Codigo/backend/src/main/java/service/PerfilService.java
+++ Codigo/backend/src/main/java/service/PerfilService.java
@@ -14,6 +14,7 @@ public class PerfilService {
     private DAOCliente daoc = new DAOCliente();
     private DAOFornecedor daof = new DAOFornecedor();
 
+    // TEM QUE ATUALIZAR A SENHA SEPARADO POR QUE GEROU HASH DO HASH NO BD
     public Object atualizar(Request request, Response response) {
         try {
             boolean resp = false;
@@ -36,7 +37,7 @@ public class PerfilService {
             }
             if (resp) {
                 return "<script src=\"" + Aplicacao.url
-                        + "/acess.js\"></script><script>alert('USUARIO ALTERADO COM SUCESSO, POR FAVOR REALIZE LOGIN NOVAMENTE!');logout();</script>";
+                        + "/js/acess.js\"></script><script>alert('USUARIO ALTERADO COM SUCESSO, POR FAVOR REALIZE LOGIN NOVAMENTE!');logout();</script>";
             } else {
                 return "<script>alert('UM ERRO IMPEDIU QUE O USUARIO FOSSE ALTERADO');window.location.href = \""
                         + Aplicacao.url + "/pages/home-usuarios.html\";</script>";
@@ -44,6 +45,44 @@ public class PerfilService {
         } catch (Exception e) {
             return "<script>alert('UM ERRO IMPEDIU QUE O USUARIO FOSSE ALTERADO');window.location.href = \""
                     + Aplicacao.url + "/pages/home-usuarios.html\";</script>";
+        }
+    }
+
+    public Object newPass(Request request, Response response) {
+        try {
+            boolean resp = false;
+            String token = request.params(":token");
+            String senha = request.queryParams("senha");
+            String cfsenha = request.queryParams("confirmSenha");
+            if (!senha.equals(cfsenha)) {
+                throw new Exception("Senhas não conferem!");
+            } else {
+                Fornecedor forn = null;
+                Cliente cli = null;
+                Object prop = new DAOToken().convertTokenCliente(token);
+                if (prop == null) {
+                    prop = new DAOToken().convertTokenFornecedor(token);
+                    forn = (Fornecedor) prop;
+                } else {
+                    cli = (Cliente) prop;
+                }
+                if (cli != null) {
+                    resp = daoc.updatePass(cli, senha);
+                } else {
+                    resp = daof.updatePass(forn, senha);
+                }
+                if (resp) {
+                    return "<script src=\"" + Aplicacao.url
+                            + "/js/acess.js\"></script><script>alert('SENHA ALTERADA COM SUCESSO, POR FAVOR REALIZE LOGIN NOVAMENTE!');onStart();window.location.href = '"
+                            + Aplicacao.url + "/login.html'</script>";
+                } else {
+                    return "<script>alert('UM ERRO IMPEDIU QUE A SENHA FOSSE ALTERADA');window.location.href = \""
+                            + Aplicacao.url + "/pages/home-usuarios.html\";</script>";
+                }
+            }
+        } catch (Exception e) {
+            return "<script>alert('UM ERRO IMPEDIU QUE A SENHA FOSSE ALTERADA - "+e.getMessage()+"');window.location.href = \""
+                    + Aplicacao.url + "/pages/home-trocar-senha.html\";</script>";
         }
     }
 
@@ -67,13 +106,13 @@ public class PerfilService {
         }
         if (resp) {
             return "<script src=\"" + Aplicacao.url
-                    + "/acess.js\"></script><script>alert('USUARIO EXCLUIDO COM SUCESSO!');logout();</script>";
+                    + "/js/acess.js\"></script><script>alert('USUARIO EXCLUIDO COM SUCESSO!');onStart();window.location.href = '"
+                    + Aplicacao.url + "'</script>";
         } else {
             return "<script>alert('UM ERRO IMPEDIU QUE O USUARIO FOSSE EXCLUIDO');window.location.href = \""
                     + Aplicacao.url + "/pages/home-usuarios.html\";</script>";
         }
     }
-
 
     public Object carregar(Request request, Response response) {
         try {
@@ -94,36 +133,38 @@ public class PerfilService {
             }
             response.status(200);
             if (cli != null) {
-                resp = "<form id=\"form-user\" action=\"/perfil/atualizar/" + token + "\" class=\"form\">" +
-                        "<div class=\"row mb-3\"><div class=\"col-6\"><label for=\"tipo\" class=\"form-label\">Tipo</label> <input type=\"text\" name=\"tipo\" id=\"tipo\" class=\"form-control\" value=\"Cliente\" required>"
+                resp = "<form id=\"form-user\" method=\"post\" action=\"/perfil/atualizar/" + token
+                        + "\" class=\"form\">" +
+                        "<div class=\"row mb-3\"><div class=\"col-6\"><label for=\"tipo\" class=\"form-label\">Tipo</label> <input type=\"text\" name=\"tipo\" id=\"tipo\" class=\"form-control\" value=\"Cliente\" disabled readonly>"
                         +
-                        "</div></div><div class=\"row mb-3\"><div class=\"col-6\"><label for=\"nome\" class=\"form-label\">Nome</label> <input type=\"text\" name=\"nome\" id=\"nome\" value=\""
+                        "<div class=\"col-6\"><label for=\"nome\" class=\"form-label\">Nome</label> <input type=\"text\" name=\"nome\" id=\"nome\" value=\""
                         + cli.getNome() + "\" class=\"form-control\">"
                         +
-                        "</div><div class=\"col-6\"><label for=\"email\" class=\"form-label\">Email</label> <input type=\"email\" name=\"email\" id=\"email\" class=\"form-control\" value=\""
-                        + cli.getEmail() + "\"></div></div>"
+                        "</div></div><div class=\"row mb-3\"><div class=\"col-6\"><label for=\"email\" class=\"form-label\">Email</label> <input type=\"email\" name=\"email\" id=\"email\" class=\"form-control\" value=\""
+                        + cli.getEmail() + "\"></div>"
                         +
-                        "<div class=\"row mb-3\"><div class=\"col-6\"><label for=\"usuario\" class=\"form-label\">Usuário</label> <input type=\"text\" readonly disabled name=\"usuario\" id=\"usuario\" class=\"form-control\" value=\""
+                        "<div class=\"col-6\"><label for=\"usuario\" class=\"form-label\">Usuário</label> <input type=\"text\" readonly disabled name=\"usuario\" id=\"usuario\" class=\"form-control\" value=\""
                         + cli.getUsuario() + "\"></div>"
                         +
-                        "</div><div class=\"row mb-4\"><div class=\"col-6 d-flex justify-content-start align-items-end\"><a href=\"./home-trocar-senha.html\"><button type=\"button\" class=\"btn btn-outline-primary p-3\">Trocar"
+                        "</div><div class=\"row mb-3\"><a href=\"./home-trocar-senha.html\"><button type=\"button\" class=\"btn btn-outline-primary p-3\">Trocar "
                         +
-                        "Senha</button></a></div></div><div class=\"row\"><div class=\"col-6\"><button type=\"submit\" class=\"btn btn-outline-success p-3\">AtualizarDados</button> </div></div>"
+                        "Senha</button></a></div><div class=\"row\"><div class=\"col-6\"><button type=\"submit\" class=\"btn btn-outline-success p-3\">Atualizar Dados</button> </div></div><br>"
                         +
-                        "<button onclick=\"pergunta('Deseja realmente excluir todos os seus dados ?','/perfil/deletar/"
+                        "<div class=\"row\"><hr><div class=\"row mb-3\"><div class=\"col-6\"><button onclick=\"pergunta('Deseja realmente excluir todos os seus dados ?','/perfil/deletar/"
                         + token
-                        + "');\" type=\"button\" class=\"btn btn-outline-danger\"> EXCLUIR PERFIL ?<i class=\"fas fa-trash\"></i></button></form>";
+                        + "');\" type=\"button\" class=\"btn btn-outline-danger\">Excluir perfil</button></div></div></div></form>";
 
             }
             if (forn != null) {
-                resp = "<form id=\"form-user\" action=\"/perfil/atualizar/" + token + "\" class=\"form\">" +
-                        "<div class=\"row mb-3\"><div class=\"col-6\"><label for=\"tipo\" class=\"form-label\">Tipo</label> <input type=\"text\" name=\"tipo\" id=\"tipo\" class=\"form-control\" value=\"Fornecedor\" required>"
+                resp = "<form id=\"form-user\" method=\"post\" action=\"/perfil/atualizar/" + token
+                        + "\" class=\"form\">" +
+                        "<div class=\"row mb-3\"><div class=\"col-6\"><label for=\"tipo\" class=\"form-label\">Tipo</label> <input type=\"text\" name=\"tipo\" id=\"tipo\" class=\"form-control\" value=\"Fornecedor\" disabled readonly>"
                         +
                         "</div></div><div class=\"row mb-3\"><div class=\"col-6\"><label for=\"nome\" class=\"form-label\">Nome</label> <input type=\"text\" name=\"nome\" id=\"nome\" value=\""
                         + forn.getNome() + "\" class=\"form-control\">"
                         +
-                        "</div><div class=\"col-6\"><label for=\"email\" class=\"form-label\">Email</label> <input type=\"email\" name=\"email\" id=\"email\" class=\"form-control\" value=\""
-                        + forn.getEmail() + "\"></div></div>"
+                        "</div><div class=\"row mb-3\"><div class=\"col-6\"><label for=\"email\" class=\"form-label\">Email</label> <input type=\"email\" name=\"email\" id=\"email\" class=\"form-control\" value=\""
+                        + forn.getEmail() + "\"></div></div></div>"
                         +
                         "<div class=\"row mb-3\"><div class=\"col-6\"><label for=\"usuario\" class=\"form-label\">Usuário</label> <input type=\"text\" readonly disabled name=\"usuario\" id=\"usuario\" class=\"form-control\" value=\""
                         + forn.getUsuario() + "\"></div>"
