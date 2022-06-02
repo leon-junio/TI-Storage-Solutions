f16c290bd245ab8560c8a0fb5956ce33b799f415
Leon Júnio Martins <juniohuy2gamer@gmail.com>
2022-06-02 17:29:33 -0300

    Correções de bugs na listagem de fornecedor e no nome de estoque

--- Codigo/backend/src/main/java/service/FornecedorService.java
+++ Codigo/backend/src/main/java/service/FornecedorService.java
@@ -42,46 +42,47 @@ public class FornecedorService {
 				}
 			}
 			String resp = "";
-			if(chk) {
-			List<model.Fornecedor> fornecedores = null;
-			fornecedores = daof.get();
-
-			if (fornecedores == null) {
-				throw new RuntimeException("Erro em tempo de execução!");
-			}
-
-			if (fornecedores.size() > 0) {
-				response.status(200);
-				for (model.Fornecedor forn : fornecedores) {
-				resp+=	"<a id=\"item-f\" href=\"mailto:"+forn.getEmail()+"\" style=\"text-decoration: none !important;\">";
-                resp+= "<div class=\"card-counter btn-secondary\">";
-                resp+= "       <div class=\"row\">";
-                resp+= "            <div class=\"col-3\">";
-                 resp+="               <i class=\"fas fa-truck\"></i>";
-                 resp+="           </div>";
-                 resp+="           <div class=\"col-9\">";
-                 resp+="               <span class=\"count-numbers\">"+forn.getNome()+"</span> <span class=\"count-name\">"+forn.getEmail()+"</span>";
-                 resp+="           </div>";
-                 resp+="</div>";
-                    resp+="</div>";
-                resp+="</a>";
+			if (chk) {
+				List<model.Fornecedor> fornecedores = null;
+				fornecedores = daof.get();
+				if (fornecedores == null) {
+					throw new RuntimeException("Erro em tempo de execução!");
+				}
+				if (fornecedores.size() > 0) {
+					response.status(200);
+					for (model.Fornecedor forn : fornecedores) {
+						resp += "<a id=\"item-f\" href=\"mailto:" + forn.getEmail()
+								+ "\" style=\"text-decoration: none !important;\">";
+						resp += "<div class=\"card-counter btn-primary-dark\">";
+						resp += "       <div class=\"row\">";
+						resp += "            <div class=\"col-3\">";
+						resp += "               <i class=\"fas fa-truck\"></i>";
+						resp += "           </div>";
+						resp += "           <div class=\"col-9\">";
+						resp += "               <span class=\"count-numbers\">" + forn.getNome()
+								+ "</span> <span class=\"count-name\">" + forn.getEmail() + "</span>";
+						resp += "           </div>";
+						resp += "</div>";
+						resp += "</div>";
+						resp += "</a>";
+						resp += "<hr>";
+					}
+				} else {
+					throw new RuntimeException("Nenhum fornecedor localizado!");
 				}
 			} else {
-				throw new RuntimeException("Erro em tempo de execução!");
-			}
-			}else {
-				throw new RuntimeException("key_failed");	
+				throw new RuntimeException("key_failed");
 			}
 			return utils.LeonAPI.stringToJson(resp);
 		} catch (RuntimeException e) {
 			response.status(203);
-			if(e.getMessage().equals("key_failed")) {
+			if (e.getMessage().equals("key_failed")) {
 				return utils.LeonAPI.stringToJson(e.getMessage());
 			}
 			return utils.LeonAPI.stringToJson("<h3>Não existem fornecedores cadastrados no sistema no momento!</h3>");
 		} catch (Exception e) {
 			response.status(203);
-			return utils.LeonAPI.stringToJson("<h3>Não existem fornecedores cadastrados no sistema no momento!</h3>");
+			return utils.LeonAPI.stringToJson("<h3>Erro no sistema por favor tente novamente mais tarde!</h3>");
 		}
 	}
 
