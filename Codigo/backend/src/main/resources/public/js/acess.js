16e60125a6ef6a07a243160e90c90a3cce1edafe
Leon Júnio Martins <juniohuy2gamer@gmail.com>
2022-06-02 17:28:40 -0300

    Sistema para salvar estoque no localstorage temporariamente

--- Codigo/backend/src/main/resources/public/js/acess.js
+++ Codigo/backend/src/main/resources/public/js/acess.js
@@ -1,33 +1,58 @@
-let usuario = {user:"null",pass:"null",token:"null"}
+let usuario = { user: "null", pass: "null", token: "null" }
+let estoque = { id: "null", nome: "null", capacidade: "null" }
 
-function writeInStorage(obj){
-	localStorage.setItem('usuario', JSON.stringify(obj));
+function writeInStorage(obj) {
+    localStorage.setItem('usuario', JSON.stringify(obj));
 }
 
-function loginSucess(res_user,res_pass,res_token){
-	usuario.user = res_user;
-	usuario.pass = res_pass;
-	usuario.token = res_token;
-	writeInStorage(usuario);
+function setEstoque(obj) {
+    localStorage.setItem('estoque', JSON.stringify(obj));
 }
 
-function readStorage(){
-	return JSON.parse(getStr());
+function loginSucess(res_user, res_pass, res_token) {
+    usuario.user = res_user;
+    usuario.pass = res_pass;
+    usuario.token = res_token;
+    writeInStorage(usuario);
 }
 
-function attObj(){
-	usuario = readStorage();
+function showEstoque(id, nome, capacidade) {
+    estoque.id = id;
+    estoque.nome = nome;
+    estoque.capacidade = capacidade;
+    setEstoque(estoque);
 }
 
-function getStr(){
-	return localStorage.getItem('usuario');
+function readStorage() {
+    return JSON.parse(getStr());
 }
 
-function onStart(){
-	writeInStorage(usuario);
+function readStorageEst() {
+    return JSON.parse(getStorage());
 }
 
-function logout(){
-	onStart();
-	window.location.href = "../index.html";
+function attObj() {
+    usuario = readStorage();
+}
+
+function attEst() {
+    estoque = readStorage();
+}
+
+function getStr() {
+    return localStorage.getItem('usuario');
+}
+
+function getStorage() {
+    return localStorage.getItem('estoque');
+}
+
+function onStart() {
+    writeInStorage(usuario);
+    setEstoque(estoque);
+}
+
+function logout() {
+    onStart();
+    window.location.href = "../index.html";
 }
